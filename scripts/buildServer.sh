f43b433c8e904a71ceab856a4b8ea429269db398
Pat Needham <pat@needham.io>
2020-05-20 01:02:13 -0400

    copy guac.jpg to dist during buildServer.sh

--- scripts/buildServer.sh
+++ scripts/buildServer.sh
@@ -5,5 +5,6 @@ npx babel build -d dist
 mkdir -p dist/assets
 cp -r public/assets/* dist/assets
 cp src/webpackAssets.json dist/webpackAssets.json
+cp src/components/pages/guac.jpg dist/components/pages/guac.jpg
 cd dist
 zip -r function.zip ./
