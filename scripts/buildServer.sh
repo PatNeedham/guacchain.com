0e40dd47fc8e56d6e73c0670bf4eb8af2ce03b9a
Pat Needham <pat@needham.io>
2020-05-20 00:40:53 -0400

    copy over App.css to dist inside buildServer.sh

--- scripts/buildServer.sh
+++ scripts/buildServer.sh
@@ -5,5 +5,6 @@ npx babel build -d dist
 mkdir -p dist/assets
 cp -r public/assets/* dist/assets
 cp src/webpackAssets.json dist/webpackAssets.json
+cp src/components/pages/App.css dist/components/pages/App.css
 cd dist
 zip -r function.zip ./
