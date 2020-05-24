4186f41d8decb26ec37adcd967bb31711421c408
Pat Needham <pat@needham.io>
2020-05-24 17:07:49 -0400

    include favicon in s3 assets

--- scripts/buildServer.sh
+++ scripts/buildServer.sh
@@ -6,5 +6,6 @@ mkdir -p dist/assets
 cp -r public/assets/* dist/assets
 cp src/webpackAssets.json dist/webpackAssets.json
 cp src/components/pages/guac.jpg dist/assets/guac.jpg
+cp src/favicon.ico dist/assets/favicon.ico
 cd dist
 zip -r function.zip ./
