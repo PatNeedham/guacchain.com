05aaafcc578eb9df8515bfc3ab5615aa65c827c3
Pat Needham <pat@needham.io>
2020-05-25 09:30:01 -0400

    add favicon to htmlTemplate server response

--- src/server.js
+++ src/server.js
@@ -27,9 +27,11 @@ export const htmlTemplate = (reactDom, styles) => {
         <!DOCTYPE html>
         <html>
         <head>
-            <meta charset="utf-8">
-            <title>Guacchain</title>
-            ${styles}
+          <title>Guacchain</title>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+          ${styles}
         </head>
 
         <body>
